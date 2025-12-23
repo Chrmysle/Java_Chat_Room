f6b3cd7debcaab4bfa236215ab2647ce5661f89d
张少 <qinghaipiaopozhangshao@gmail.com>
2025-12-23 20:16:54 +0800

    清空 main 分支所有文件

--- box-im-project/im-platform/src/main/java/com/bx/implatform/mapper/BlogPostMapper.java
+++
@@ -1,9 +0,0 @@
-package com.bx.implatform.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.bx.implatform.entity.BlogPost;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface BlogPostMapper extends BaseMapper<BlogPost> {
-}
